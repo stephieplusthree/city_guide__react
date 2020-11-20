f82834f521239d9805fa31d748e1549c6b9a0776
stephanie <stephieplusthree@gmail.com>
2020-11-20 15:46:16 -0800

    data showing in cards

--- src/components/context.js
+++ src/components/context.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { placeInfo, reviews, detailInfo, news } from '../data';
 
 const InfoContext = React.createContext();
 
-// Provider
-// Consumer
-
 class InfoProvider extends Component {
+
+    state = {
+        info: placeInfo,
+        reviews: reviews,
+        detailInfo: detailInfo,
+        new: news 
+    }
+
     render() {
         return (
-            <InfoContext.Provider value="Hello I am a DATA">
+            <InfoContext.Provider value={{
+                info: this.state.info,
+                reviews: this.state.reviews,
+                maps: this.state.maps,
+                headerTitle: this.state.headerTitle,
+                headerSubTitle: this.state.headerSubTitle,
+                headerText: this.state.headerText,
+                detailInfo: this.state.detailInfo,
+                news: this.state.news,
+                name: this.state.name,
+                avatar: this.state.avatar,
+                comment: this.state.comment
+            }}>
                 {this.props.children}
             </InfoContext.Provider>
         )
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { InfoConsumer } from '../context';
-import Footer from '../layouts/Footer';
-
+import React from "react";
+import { InfoConsumer } from "../context";
+import Footer from "../layouts/Footer";
+import Info from "../Info";
 
 function Home() {
-    return (
-        <div>
-            <InfoConsumer>
-                {data => {
-                    return <h2>{data}</h2>
-                }}
-            </InfoConsumer>
-            <Footer />
+  return (
+    <div>
+      <div className="container">
+        <div className="row mt-5">
+          <InfoConsumer>
+            {(value) => {
+              return value.info.map(item => {
+                  return <Info key={item.id} item={item} />
+              })
+            }}
+          </InfoConsumer>
         </div>
-    )
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
